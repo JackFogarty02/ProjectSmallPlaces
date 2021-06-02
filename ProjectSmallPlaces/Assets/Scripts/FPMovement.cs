4f5399e46436258bbb166bc68ede60b13b7d8ea2
Jack Fogarty <s210196@students.aie.edu.au>
2021-06-02 16:43:05 +1000

    LevelCreated

---
+++ ProjectSmallPlaces/Assets/Scripts/FPMovement.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+//NOTE: parts of this code were taken from a handout project, all additions will be makred with a "//Added"
+public class FPMovement : MonoBehaviour
+{
+    //Variables
+    public UnityEngine.CharacterController m_charController;
+    //Movement
+    public float m_movementSpeed = 12f;
+    public float m_runSpeed = 1.5f;
+
+    //Gravity&physics
+    public float m_gravity = -9.81f;
+    public float m_jumpHeight = 3f;
+    private Vector3 m_velocity;
+
+    //Ground
+    public Transform m_groundCheckPoint;
+    public float m_groundDistance = 0.4f;
+    public LayerMask m_groundMask;
+    private bool m_isGrounded;
+
+    //Inputs
+    public KeyCode m_forward;
+    public KeyCode m_back;
+    public KeyCode m_left;
+    public KeyCode m_right;
+    public KeyCode m_sprint;
+    public KeyCode m_jump;
+    public KeyCode m_interact; //Added
+
+    //final speed
+    private float m_finalSpeed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_finalSpeed = m_movementSpeed; //Resets the base movement speed
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_isGrounded = HitGroundCheck(); //Every frame the program will check if the player is on the ground
+        MoveInputCheck();//Every frame the program will check if the player hsa pressed any of the assigned movement inputs
+    }
+
+    //Check if a button is pressed
+    void MoveInputCheck()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = Vector3.zero; //Set the vectors to 0
+
+        if (Input.GetKey(m_forward) || Input.GetKey(m_back) || Input.GetKey(m_left) || Input.GetKey(m_right))
+        {
+            move = transform.right * x + transform.forward * z;
+        }
+
+        MovePlayer(move);
+        RunCheck(); //checks of the player has inputted the assigned run key
+        JumpCheck(); //checks if the player has inputted the assigned jump key
+    }
+
+    //MovePlayer
+    void MovePlayer(Vector3 move)
+    {
+        m_charController.Move(move * m_finalSpeed * Time.deltaTime); //moves the Gameobject using the Character Controller
+
+        m_velocity.y += m_gravity * Time.deltaTime; //Gravity affects the jump velocity
+        m_charController.Move(m_velocity * Time.deltaTime); //Actually move the player up
+    }
+
+    //Player run
+    void RunCheck()
+    {
+        if (Input.GetKeyDown(m_sprint)) // if the assigned key is held down, sprint
+        {
+            m_finalSpeed = m_movementSpeed * m_runSpeed;
+        }
+        else if (Input.GetKeyUp(m_sprint)) // IF the assigned key is not held down,  don't sprint
+        {
+            m_finalSpeed = m_movementSpeed;
+        }
+    }
+
+    //Ground check
+    bool HitGroundCheck()
+    {
+        bool isGrounded = Physics.CheckSphere(m_groundCheckPoint.position, m_groundDistance, m_groundMask);
+
+        //Gravity
+        if (isGrounded && m_velocity.y < 0)
+        {
+            m_velocity.y = -2f;
+        }
+
+        return isGrounded;
+    }
+
+    //Jump Check
+    void JumpCheck()
+    {
+        if (Input.GetKeyDown(m_jump)) //If the jump key is pressed and the player is grounded
+        {
+            if (m_isGrounded == true)
+            {
+                m_velocity.y = Mathf.Sqrt(m_jumpHeight * -2f * m_gravity); //change object position
+            }
+        }
+    }
+}
---
+++ ProjectSmallPlaces/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float m_sensitivity = 100f; //Mpouse senitivity
+    public float m_clampAngle = 90f; //This limits the camera's vertical rotation
+    public Transform m_playerObject; //Store the player controller
+    public Transform m_camera; //Store the player camera
+
+    private Vector2 m_mousePos; //Store mouse position
+    private float m_xRotation = 0f; //final loop up rotation value
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; //Lock the cursor to the center of the screen
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetMousePos(); //Get the moues position
+        ClampUpRotation(); //Clamp the loop up
+        LookAt(); //look at mouse position
+    }
+
+    //Get the moues position
+    private void GetMousePos() //Get the mouses current posiiton on the x and y axis
+    {
+        m_mousePos.x = Input.GetAxis("Mouse X") * m_sensitivity * Time.deltaTime;
+        m_mousePos.y = Input.GetAxis("Mouse Y") * m_sensitivity * Time.deltaTime;
+    }
+
+    //FixRotation
+    private void ClampUpRotation()
+    {
+        m_xRotation -= m_mousePos.y;
+        m_xRotation = Mathf.Clamp(m_xRotation, -m_clampAngle, m_clampAngle);
+    }
+
+    //Look at the mouse position
+    private void LookAt()
+    {
+        m_camera.transform.localRotation = Quaternion.Euler(m_xRotation, 0f, 0f);//move the z axis to match the mouse's position
+        m_playerObject.Rotate(Vector3.up * m_mousePos.x); //multiply the vector by the x axis value of the mouse input
+    }
+}
